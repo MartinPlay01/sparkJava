f17b5195aed50d3e050061617527aaac7f81c992
MartinPlay01 <nitram2435@gmail.com>
2020-02-23 15:28:44 +0100

    Unimos imagen PostgreSQL 9.3

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 first commit...
+
+##Como instalar el postgres
+
+1) Creamos la imagen
+
+docker build -t userHub/repository:tag .
+
+2) Arrancamos el contenedor
+
+docker run -d --name containerName -p 5432:5432 userHub/repository:tag
+
+docker exec -it containerName bash
--- projectOne/src/main/java/csvToDb.java
+++ projectOne/src/main/java/csvToDb.java
@@ -1,29 +1,45 @@
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
+
+import java.util.Properties;
+
 import static org.apache.spark.sql.functions.concat;
 import static org.apache.spark.sql.functions.lit;
 
 public class csvToDb {
-
-    public static void main(String[] args) {
-
-        SparkSession spark = SparkSession.builder()
-                .appName("csv to DB")
-                .master("local")
-                .getOrCreate();
-
-        //get data
-        Dataset<Row> df = spark.read().format("csv")
-                .option("header", true)
-                .load("src/main/resources/name_and_comments.txt");
-
-
-        Dataset<Row> df2 = df.withColumn("fullname", concat(df.col("first_name"), lit(", ") , df.col("last_name")));
-
-        df2 = df2.filter(df.col("comment").rlike("\\d+")).orderBy(df.col("last_name").asc());
-
-        df2.show(5);
-    }
-
+  
+  public static void main(String[] args) {
+    
+    SparkSession spark = SparkSession.builder()
+      .appName("csv to DB")
+      .master("local")
+      .getOrCreate();
+    
+    //get data
+    Dataset<Row> df = spark.read().format("csv")
+      .option("header", true)
+      .load("src/main/resources/name_and_comments.txt");
+    
+    df = df.withColumn("fullname",
+      concat(df.col("first_name"), lit(", "), df.col("last_name")))
+      .filter(df.col("comment").rlike("\\d+"))
+      .orderBy(df.col("last_name").asc());
+    
+    db.show(5)
+    
+    
+    String dbConnectionUrl = "jdbc:postgresql://localhost/course_data";
+    Properties prop = new Properties();
+    prop.setProperty("driver", "org.postgresql.Driver");
+    prop.setProperty("user", "postgres");
+    prop.setProperty("password", "postgres");
+    
+    
+    //    df.write()
+    //            .mode(SaveMode.Overwrite)
+    //            .jdbc(dbConnectionUrl, "projectOne", prop);
+  }
+  
 }
