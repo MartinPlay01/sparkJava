3a0a3f5ca55bf8cb594e8d3d768f658eedf6ef2b
Martín <mizquierdo@paradigmadigital.com>
2020-04-04 21:30:08 +0200

    borrando csv

---
+++ projectSeven/src/main/java/gotostreaming/StreamingKafkaConsumer.java
@@ -0,0 +1,46 @@
+package gotostreaming;
+
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.streaming.StreamingQuery;
+import org.apache.spark.sql.streaming.StreamingQueryException;
+
+import java.util.Arrays;
+
+public class StreamingKafkaConsumer {
+  
+  public static void main(String[] args) throws StreamingQueryException {
+    SparkSession spark = SparkSession.builder()
+      .appName("Streaming kafka consumer")
+      .master("local")
+      .getOrCreate();
+    
+    
+//    Kafka consumer
+    Dataset<Row> messageDf = spark.readStream()
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092,localhost:9093,localhost:9094")
+      .option("subscribe", "test")
+      .load()
+      .selectExpr("CAST(value AS STRING)");// lines.selectExpr("CAST key AS STRING", "CAST value AS STRING") for key value
+    
+//    messageDf.show() -> can't do this when streaming!
+    Dataset<String> words = messageDf
+      .as(Encoders.STRING())
+      .flatMap((FlatMapFunction<String, String>) x -> Arrays.asList(x.split(" ")).iterator(), Encoders.STRING());
+    
+    Dataset<Row> wordCount = words.groupBy("value").count();
+  
+  
+    StreamingQuery query = wordCount.writeStream()
+      .outputMode("complete")
+      .format("console")
+      .start();
+    
+    query.awaitTermination();
+    
+  }
+}
---
+++ projectSix/src/test/java/Application.java
@@ -0,0 +1,60 @@
+import gosparkwithjava.WordUtils;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import static org.apache.spark.sql.functions.*;
+
+import java.util.Arrays;
+
+public class Application {
+  
+  public static void main(String[] args) {
+    SparkSession spark = SparkSession.builder()
+      .appName("Reddit Analyze comments")
+      .getOrCreate();
+//      .master("local")//remove this line for execute in distributed mode
+
+
+//    add uri for s3
+
+    String reddit_file = "s3://myspark-course-bucket/RC_2011-12";
+//    String reddit_file2 = "/home/mizquierdo/Documentos/cursos/sparkIntellij/data/RC_2011-12";
+//    String reddit_file = "/home/mizquierdo/Documentos/cursos/sparkIntellij/data/reddit_2007-01-small";
+    
+    Dataset<Row> redditDf = spark.read().format("json")
+      .option("inferSchema", "true")
+      .option("header", true)
+      .load(reddit_file);
+    
+    redditDf = redditDf.select("body");
+    
+    Dataset<String> wordsDs = redditDf.flatMap((FlatMapFunction<Row, String>)
+        r -> Arrays.asList(r.toString().replace("\n", "")
+          .replace("\r", "")
+          .trim().toLowerCase()
+          .split(" ")).iterator()
+      , Encoders.STRING());
+    
+    
+    Dataset<Row> wordsDf = wordsDs.toDF();
+    
+    Dataset<Row> wordStop = spark
+      .createDataset(Arrays.asList(WordUtils.stopWords)
+        , Encoders.STRING()).toDF();
+    
+//    wordsDf = wordsDf.except(wordStop); // <--- this remove duplicates from the end dataframe
+    wordsDf = wordsDf.join(wordStop, wordsDf.col("value").equalTo(wordStop.col("value")), "leftanti");
+    
+    wordsDf = wordsDf.groupBy("value").count();
+    
+    wordsDf.orderBy(desc("count"));
+    
+    
+    wordsDf.printSchema();
+    
+    wordsDf.show(3, 90);
+  }
+}
